9f3755295f042db63775bf5b4fff7c303448d52e
lxbzmy <lxbzmy@gmail.com>
2019-05-12 15:58:01 +0800

    Clojure hello

---
+++ clojure-hello/src/main/clojure/Hello.clj
@@ -0,0 +1,6 @@
+; License Apache License, Version 2.0
+; @author lxb
+(ns Hello
+      (:gen-class))
+
+(defn -main [] (println "Hello, World!"))
---
+++ clojure-hello/src/test/clojure/Basic.clj
@@ -0,0 +1,10 @@
+; License Apache License, Version 2.0
+; @author lxb
+(ns Basic
+  (:use
+   clojure.test)
+  )
+
+(deftest atest
+  (is (= 0 0))
+  )
